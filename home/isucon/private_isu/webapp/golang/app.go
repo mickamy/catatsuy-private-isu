fbe9f001e84169ae1ceb202d706b50eee83df558
Tetsuro Mikami <tetsuro@mickamy.com>
2026-05-04 15:11:52 +0900

    app: write uploaded image synchronously

--- home/isucon/private_isu/webapp/golang/app.go
+++ home/isucon/private_isu/webapp/golang/app.go
@@ -893,10 +893,11 @@ func postIndex(w http.ResponseWriter, r *http.Request) {
 		"image/gif":  "gif",
 	}[mime]
 	if ext != "" {
+		// goroutine で逃すと redirect 後の GET /image/{pid} が write 未完了の
+		// 部分ファイルを掴んで bench が「静的ファイルが正しくありません」で
+		// fail する。同期書き込みで完了を保証する。
 		imgFilePath := fmt.Sprintf("%s/%d.%s", imageDir, pid, ext)
-		go func(path string, data []byte) {
-			_ = os.WriteFile(path, data, 0644)
-		}(imgFilePath, filedata)
+		_ = os.WriteFile(imgFilePath, filedata, 0644)
 	}
 
 	http.Redirect(w, r, "/posts/"+strconv.FormatInt(pid, 10), http.StatusFound)

edb57cffc0b8820e930b008a9e56d881d167ea69
Tetsuro Mikami <tetsuro@mickamy.com>
2026-05-04 13:33:20 +0900

    fix: access to 3306

--- home/isucon/private_isu/webapp/golang/app.go
+++ home/isucon/private_isu/webapp/golang/app.go
@@ -869,10 +869,7 @@ func main() {
 	if host == "" {
 		host = "localhost"
 	}
-	port := os.Getenv("ISUCONP_DB_PORT")
-	if port == "" {
-		port = "3306"
-	}
+	port := "3306"
 	_, err := strconv.Atoi(port)
 	if err != nil {
 		log.Fatalf("Failed to read DB port number from an environment variable ISUCONP_DB_PORT.\nError: %s", err.Error())

14af1c442926bec425d3407de3919d230fe11c96
Tetsuro Mikami <tetsuro@mickamy.com>
2026-05-04 12:09:08 +0900

    fix: add table alias to select

--- home/isucon/private_isu/webapp/golang/app.go
+++ home/isucon/private_isu/webapp/golang/app.go
@@ -476,7 +476,7 @@ func getIndex(w http.ResponseWriter, r *http.Request) {
 	results := []Post{}
 
 	err := db.Select(&results, `
-SELECT id, user_id, body, mime, created_at
+SELECT p.id, p.user_id, p.body, p.mime, p.created_at
 FROM posts p
 JOIN users u ON p.user_id = u.id
 WHERE u.del_flg = 0
